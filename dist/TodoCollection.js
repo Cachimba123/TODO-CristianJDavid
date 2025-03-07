e97e10631fba6a298b1f6a669df296267c074b88
Cachimba123 <jesuscda123@gmail.com>
2025-03-06 20:03:44 -0500

    Actalizacion pro

--- dist/TodoCollection.js
+++ dist/TodoCollection.js
@@ -1,51 +1,46 @@
-import { TodoItem } from "./TodoItem";
-
-type ItemCounts = {
-    total: number;
-    incomplete: number;
-}
-
-export class TodoCollection {
-    private nextId: number = 1;
-    private itemMap= new Map<number, TodoItem>();
-
-    constructor(public userName: string, public todoItems: TodoItem[] = []) {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.TodoCollection = void 0;
+const TodoItem_1 = require("./TodoItem");
+class TodoCollection {
+    constructor(userName, todoItems = []) {
+        this.userName = userName;
+        this.todoItems = todoItems;
+        this.nextId = 1;
+        this.itemMap = new Map();
         todoItems.forEach(item => this.itemMap.set(item.id, item));
     }
-
-    addTodo(task: string): number {
+    addTodo(task) {
         while (this.getTodoById(this.nextId)) {
             this.nextId++;
         }
-        this.itemMap.set(this.nextId, new TodoItem(this.nextId, task));
+        this.itemMap.set(this.nextId, new TodoItem_1.TodoItem(this.nextId, task));
         return this.nextId;
     }
-    getTodoById(id: number) : TodoItem {
+    getTodoById(id) {
         return this.itemMap.get(id);
     }
-
-    getTodoItems(includeComplete: boolean): TodoItem[] {
+    getTodoItems(includeComplete) {
         return [...this.itemMap.values()].filter(item => includeComplete || !item.complete);
     }
-    markComplete(id: number, complete: boolean) {
+    markComplete(id, complete) {
         const todoItem = this.getTodoById(id);
         if (todoItem) {
             todoItem.complete = complete;
         }
     }
-
     removeComplete() {
         this.itemMap.forEach(item => {
             if (item.complete) {
                 this.itemMap.delete(item.id);
             }
-        })
+        });
     }
-
-    getItemsCount(): ItemCounts {
+    getItemsCount() {
         return {
             total: this.itemMap.size,
             incomplete: this.getTodoItems(false).length
         };
     }
 }
+exports.TodoCollection = TodoCollection;
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import { TodoItem } from "./todoItem";
+import { TodoItem } from "./TodoItem";
 import { TodoCollection } from "./todoCollection";
 let todos = [
- new TodoItem(1, "Buy Flowers"), new TodoItem(2, "Get Shoes"),
- new TodoItem(3, "Collect Tickets"), new TodoItem(4, "Call Joe", true)];
+    new TodoItem(1, "Buy Flowers"), new TodoItem(2, "Get Shoes"),
+    new TodoItem(3, "Collect Tickets"), new TodoItem(4, "Call Joe", true)];
 let collection = new TodoCollection("Adam", todos);
+
 console.clear();
-console.log(`${collection.userName}'s Todo List`);
-let newId = collection.addTodo("Go for run");
-let todoItem = collection.getTodoById(newId);
-todoItem.printDetails();
+console.log(`${collection.userName}'s Todo List` + `(${collection.getItemsCount().incomplete} items to do)`);
+//collection.removeComplete()
+collection.getTodoItems(true).forEach(item => item.printDetails())
